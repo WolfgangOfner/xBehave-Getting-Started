fc43bd53e42842a97721d332408e98ff9ab29c6a
Wolfgang <Wolfgang@ProgrammingWithWolfgang.com>
2019-01-30 23:36:32 +0100

    added skipped test

--- CompanyManagement.Spec/EmployeeSpecificationTests.cs
+++ CompanyManagement.Spec/EmployeeSpecificationTests.cs
@@ -19,7 +19,8 @@ namespace CompanyManagement.Spec
         [Example(150, 20, 30, 1500, 1800, 2400)]
         [Example(100, 0, 0, 1000, 1000, 1000)]
         [Example(50, 30, 50, 500, 950, 1950)]
-        public void CalculateMonthlySalary(int hoursWorkedDuringTheWeek, int hoursWorkedOntheWeekend, int hoursWorkedOnHolidays, decimal expectedSalaryForWeekHours, decimal expectedSalaryWithWeekendHours, decimal expectedTotalSalary)
+        public void CalculateMonthlySalary(int hoursWorkedDuringTheWeek, int hoursWorkedOntheWeekend, int hoursWorkedOnHolidays,
+            decimal expectedSalaryForWeekHours, decimal expectedSalaryWithWeekendHours, decimal expectedTotalSalary)
         {
             "When I add the hours worked during the week"
                 .x(() => _employee.AddMonthlyHoursWorkedDuringTheWeek(hoursWorkedDuringTheWeek));
@@ -38,6 +39,12 @@ namespace CompanyManagement.Spec
 
             $"Then the total salary should be {expectedTotalSalary}"
                 .x(() => _employee.Salary.Should().Be(expectedTotalSalary));
+        }
+
+        [Scenario(Skip = "This Scenario is skipped")]
+        public void SkippedScenario()
+        {
+            // implement test here and remove skip
         }
     }
 }
